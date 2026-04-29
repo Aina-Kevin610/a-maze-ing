84311f8d6e08c1004973d1780f574b2d5adad4ed
Aina-Kevin610 <randrianambinintsoaainakevin@gmail.com>
2026-04-29 22:08:09 +0300

    feat:draw_maze_class

--- render.py
+++ render.py
@@ -2,7 +2,7 @@ from mlx import Mlx
 import sys
 
 class Drawing:
-    def __init__(self, maze, color) -> None:
+    def __init__(self, maze, hexa_maze, color) -> None:
         h_win = 720
         w_win = 720
         self.m = Mlx()
@@ -14,6 +14,7 @@ class Drawing:
         self.cell_size_h = h_win // maze.height
         self.color = color
         self.maze = maze
+        self.hexa_maze = hexa_maze
         self.m.mlx_hook(self.win, 2, 1, self.exit_win, [self])
 
 
@@ -66,3 +67,59 @@ class Drawing:
             self.m.mlx_destroy_window(self.mlx, self.win)
             self.m.mlx_loop_exit(self.mlx)
         return 0
+    
+    def draw_maze(self):
+        x, y = 0, 0
+        for i in range(0, self.maze.height):
+            x = 0
+            for j in range(0, self.maze.width):
+                cell = int(self.hexa_maze[i][j], 16)
+                if cell == 0b0001:
+                    self.west(x, y)
+                elif cell == 0b0010:
+                    self.south(x, y)
+                elif cell == 0b0011:
+                    self.south(x, y)
+                    self.west(x, y)
+                elif cell == 0b0100:
+                    self.east(x, y)
+                elif cell == 0b0101:
+                    self.east(x, y)
+                    self.west(x, y)
+                elif cell == 0b0110:
+                    self.east(x, y)
+                    self.south(x, y)
+                elif cell == 0b0111:
+                    self.east(x, y)
+                    self.south(x, y)
+                    self.west(x, y)
+                elif cell == 0b1000:
+                    self.north(x, y)
+                elif cell == 0b1001:
+                    self.north(x, y)
+                    self.west(x, y)
+                elif cell == 0b1010:
+                    self.north(x, y)
+                    self.south(x, y)
+                elif cell == 0b1011:
+                    self.north(x, y)
+                    self.south(x, y)
+                    self.west(x, y)
+                elif cell == 0b1100:
+                    self.north(x, y)
+                    self.east(x, y)
+                elif cell == 0b1101:
+                    self.north(x, y)
+                    self.east(x, y)
+                    self.west(x, y)
+                elif cell == 0b1110: 
+                    self.north(x, y)
+                    self.east(x, y)
+                    self.south(x, y)
+                elif cell == 0b1111: 
+                    self.north(x, y)
+                    self.east(x, y)
+                    self.south(x, y)
+                    self.west(x, y)
+                x += self.cell_size_w
+            y += self.cell_size_h
